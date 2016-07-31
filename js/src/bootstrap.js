6d643e1ec08da20e90195ae5389a4e68ac1b58c7
Wupeng <425664759@qq.com>
2016-07-31 23:21:50 +0800

    Site updated: 2016-07-31 23:21:48

--- js/src/bootstrap.js
+++ js/src/bootstrap.js
@@ -10,17 +10,103 @@ $(document).ready(function () {
 
   NexT.utils.registerBackToTop();
 
+  /*
   $('.site-nav-toggle button').on('click', function () {
     var $siteNav = $('.site-nav');
     var ON_CLASS_NAME = 'site-nav-on';
     var isSiteNavOn = $siteNav.hasClass(ON_CLASS_NAME);
     var animateAction = isSiteNavOn ? 'slideUp' : 'slideDown';
     var animateCallback = isSiteNavOn ? 'removeClass' : 'addClass';
-
+    
     $siteNav.stop()[animateAction]('fast', function () {
       $siteNav[animateCallback](ON_CLASS_NAME);
     });
+
   });
+  */
+
+  var SIDEBAR_WIDTH = '320px';
+  var SIDEBAR_DISPLAY_DURATION = 200;
+
+  var siteNevToggleMotion = {
+    toggleEl: $('.site-nav-toggle button'),
+    sidebarEl: $('.sidebar'),
+    containerEl: $('.container'),
+    isSidebarVisible: false,
+    init: function () {
+      this.toggleEl.on('click', this.clickHandler.bind(this));
+      this.containerEl.on('mousedown', this.mouseDownHandler.bind(this));
+
+      $(document)
+        .on('sidebar.isHiding', function () {
+        });
+    },
+    clickHandler: function () {
+      this.isSidebarVisible ? this.hideSidebar() : this.showSidebar();
+      this.isSidebarVisible = !this.isSidebarVisible;
+    },
+    mouseDownHandler: function (e) {
+      if (this.isSidebarVisible) {
+        //alert($(window).width())
+        var bound = $(window).width() - this.sidebarEl.width();
+        if((bound - e.pageX)>5)
+        {
+          this.hideSidebar();
+          this.isSidebarVisible = !this.isSidebarVisible;
+        }
+        /*
+        else
+        {
+          alert(bound);
+          alert(e.pageX);
+        }
+        */
+      }
+    },
+    showSidebar: function () {
+      var self = this;
+
+      this.sidebarEl.velocity('stop').velocity({
+          width: SIDEBAR_WIDTH
+        }, {
+          display: 'block',
+          duration: SIDEBAR_DISPLAY_DURATION,
+          begin: function () {
+            $('.sidebar .motion-element').velocity(
+              'transition.slideRightIn',
+              {
+                stagger: 50,
+                drag: true,
+                complete: function () {
+                  self.sidebarEl.trigger('sidebar.motion.complete');
+                }
+              }
+            );
+          },
+          complete: function () {
+            self.sidebarEl.addClass('sidebar-active');
+            self.sidebarEl.trigger('sidebar.didShow');
+          }
+        }
+      );
+
+      this.sidebarEl.trigger('sidebar.isShowing');
+    },
+    hideSidebar: function () {
+
+      this.sidebarEl.find('.motion-element').velocity('stop').css('display', 'none');
+      this.sidebarEl.velocity('stop').velocity({width: 0}, {display: 'none'});
+
+      this.sidebarEl.removeClass('sidebar-active');
+      this.sidebarEl.trigger('sidebar.isHiding');
+    }
+  };
+  siteNevToggleMotion.init();
+
+
+
+
+
 
 
   CONFIG.fancybox && NexT.utils.wrapImageWithFancyBox();
